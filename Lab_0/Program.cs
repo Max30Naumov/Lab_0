94566808cf34f5691d7270c96755dd257510ed16
Max30Naumov <101650471+Max30Naumov@users.noreply.github.com>
2023-09-14 21:42:26 +0300

    Added comments

--- Lab_0/Program.cs
+++ Lab_0/Program.cs
@@ -6,24 +6,28 @@
     {
         static void Main()
         {
-            // Создаем объект Random для генерации случайных чисел
             Random random = new Random();
 
             // Генерируем два случайных целых числа
-            int num1 = random.Next(1, 11); // Генерируем число от 1 до 10
-            int num2 = random.Next(1, 11); // Генерируем число от 1 до 10
+            int num1 = random.Next(1, 11); 
+            int num2 = random.Next(1, 11); 
 
             // Вызываем функцию SquareProduct и выводим результат на консоль
             int result = SquareProduct(num1, num2);
             Console.WriteLine("Квадрат произведения {0} и {1} равен {2}", num1, num2, result);
         }
 
-        // Функция для возврата квадрата произведения двух целых чисел
+        /// <summary>
+        /// 12. Реализовать функцию возведения в квадрат произведения двух целых чисел
+        /// </summary>
+        /// <param name="num1">Первый множитель</param>
+        /// <param name="num2">Второй множитель</param>
+        /// <returns>возвращает результат возведения в квадрат произведения двух целых чисел</returns>
         static int SquareProduct(int num1, int num2)
         {
-            int product = num1 * num2; // Вычисляем произведение
-            int square = product * product; // Возводим произведение в квадрат
-            return square; // Возвращаем результат
+            int product = num1 * num2; 
+            int square = product * product; 
+            return square; 
         }
     }
 
